5345b409300f19861a8ed1deb98cf776fe24cd49
PedroCampos93 <pedrohcampos93@gmail.com>
2023-03-22 11:24:59 -0300

    adicionado api de mapa fixo nas paginas de nivel tres

---
+++ level_three_pages/js/mapa.js
@@ -0,0 +1,65 @@
+function MapaTangara(){
+
+  var url = "https://www.mapquestapi.com/staticmap/v5/map?locations=-22.9831483,-43.2109366&size=1000,200@2x&key=t7tejFGTBqNqdrN1rDQ52druTXTKwRn2";
+
+    var imagem = document.createElement("img");
+    imagem.src = url;
+
+    document.getElementById("divMapa").appendChild(imagem);
+
+}
+
+function MapaCopacabana(){
+
+  var url = "https://www.mapquestapi.com/staticmap/v5/map?locations=-22.9673061,-43.1811428&size=1000,200@2x&key=t7tejFGTBqNqdrN1rDQ52druTXTKwRn2";
+
+    var imagem = document.createElement("img");
+    imagem.src = url;
+
+    document.getElementById("divMapa").appendChild(imagem);
+
+}
+
+function MapaFushi(){
+
+  var url = "https://www.mapquestapi.com/staticmap/v5/map?locations=3.8849576,72.7263192&size=1000,200@2x&key=t7tejFGTBqNqdrN1rDQ52druTXTKwRn2";
+
+    var imagem = document.createElement("img");
+    imagem.src = url;
+
+    document.getElementById("divMapa").appendChild(imagem);
+
+}
+
+function MapaTupana(){
+
+  var url = "https://www.mapquestapi.com/staticmap/v5/map?locations=-4.181464,-60.7769707&size=1000,200@2x&key=t7tejFGTBqNqdrN1rDQ52druTXTKwRn2";
+
+    var imagem = document.createElement("img");
+    imagem.src = url;
+
+    document.getElementById("divMapa").appendChild(imagem);
+
+}
+
+function MapaHib3(){
+
+  var url = "https://www.mapquestapi.com/staticmap/v5/map?locations=-22.9831483,-43.2109366&size=1000,200@2x&key=t7tejFGTBqNqdrN1rDQ52druTXTKwRn2";
+
+    var imagem = document.createElement("img");
+    imagem.src = url;
+
+    document.getElementById("divMapa").appendChild(imagem);
+
+}
+
+function MapaHmsp(){
+
+  var url = "https://www.mapquestapi.com/staticmap/v5/map?locations=-23.5594745,-46.667981&size=1000,200@2x&key=t7tejFGTBqNqdrN1rDQ52druTXTKwRn2";
+
+    var imagem = document.createElement("img");
+    imagem.src = url;
+
+    document.getElementById("divMapa").appendChild(imagem);
+
+}
